d20ef0e7e7f9f1b4655b27381a169328c4c4fc42
Konstantin Nekhotuev <k.nekhotuev@gmail.com>
2018-12-30 10:21:18 +0300

    Add validation to all viewmodels

--- Web/ViewModel/FlightViewModel.cs
+++ Web/ViewModel/FlightViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using Core.Model;
 
 namespace Web.ViewModel
@@ -9,19 +10,49 @@ namespace Web.ViewModel
     {
         public int Id { get; set; }
         public string Code { get; set; }
+
+        [Required]
+        [Range(0, 1000000, ErrorMessage = "Out of range 0-1 000 000")]
         [DataType(DataType.Currency)]
         public decimal TicketPrice { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Out of range 0-100")]
         public int MaxDiscount { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{4}-\d{2}-\d{2}", ErrorMessage = "Format must be yyyy-mm-dd")]
         [DataType(DataType.Date)]
         public int SaleStartDay { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{4}-\d{2}-\d{2}", ErrorMessage = "Format must be yyyy-mm-dd")]
         [DataType(DataType.Date)]
         public int SaleEndDay { get; set; }
+
+        [Required]
+        [Range(0,200000, ErrorMessage = "Out of range 0-200000")]
         public int DistanceInMile { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{4}-\d{2}-\d{2}-\d{2}-\d{2}", ErrorMessage = "Format must be yyyy-mm-dd-hh-mm")]
         [DataType(DataType.Time)]
         public DateTime TimeOfFlight { get; set; }
+
+        [Required]
+        [Range(0, 400, ErrorMessage = "Out of range 1-400")]
         public int LuggageAmount { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Out of range 1-100")]
         public int LuggageMaxWeight { get; set; }
+
+        [Required]
+        [Range(0, 400, ErrorMessage = "Out of range 1-400")]
         public int HandLuggageAmount { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Out of range 1-100")]
         public int HandLuggageMaxWeight { get; set; }
 
         public int ArrivalAirportSchemeId { get; set; }
--- Web/ViewModel/PlaneViewModel.cs
+++ Web/ViewModel/PlaneViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Core.Model;
 
 namespace Web.ViewModel
@@ -6,17 +7,53 @@ namespace Web.ViewModel
     public class PlaneViewModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9]", ErrorMessage = "Must contain only chars and numbers")]
         public string Manufacturer { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9]", ErrorMessage = "Must contain only chars and numbers")]
         public string PlaneModel { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9]", ErrorMessage = "Must contain only chars and numbers")]
         public string PlaneModification { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Out of range 0-1000")]
         public int FirstBCSeatRow { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Out of range 0-1000")]
         public int LastBCSeatRow { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]", ErrorMessage = "Must contain only chars")]
         public string FirstBCSeat { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]", ErrorMessage = "Must contain only chars")]
         public string LastBCSeat { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Out of range 0-1000")]
         public int FirstECSeatRow { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Out of range 0-1000")]
         public int LastECSeatRow { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]", ErrorMessage = "Must contain only chars")]
         public string FirstECSeat { set; get; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]", ErrorMessage = "Must contain only chars")]
         public string LastECSeat { get; set; }
+
+        [Required]
+        [Range(0, 10000, ErrorMessage = "Out of range 0-10000")]
         public double FuelConsumptionPerKm { get; set; }
 
         public virtual ICollection<Flight> Flights { get; set; }
